c46b7a2237cd10bc3de57523feb8ef3b7f9b5c9e
joximaran <80597777+joximaran@users.noreply.github.com>
2024-12-02 22:53:12 +0000

    agrego operadores

--- ada-js/clase-8/ejercicios/2.tipo-datos.js
+++ ada-js/clase-8/ejercicios/2.tipo-datos.js
@@ -1,16 +1,15 @@
-//Defino distintos tipos de datos
+// Defino distintos tipos de datos
 let numero = 18; // Numero entero
 let numero2 = 18.23; // Numero decimal
-let texto = "Hola universo"; //es un string puede ser con comillas dobles o simples
-let booleano = false; // Tambien puede ser true
-let undefined =; //Indefinido
-let nulo = null; // Valor vacio
+let texto = "Hola universo"; // es un string puede ser con comillas dobles o simples
+let booleano = false; // También puede ser true
+let sindefinir; // Indefinido (sin asignar valor)
+let nulo = null; // Valor vacío
 
 console.log(`Numeros: ${numero} y ${numero2}`);
 console.log(`Numero: ${numero}`);
-console.log(`texto: ${texto}`);
+console.log(`Texto: ${texto}`);
 console.log(`Booleano: ${booleano}`);
-console.log(`Sin definir: ${undefined}`);
-console.log(`Nulo: ${null}`);
-
+console.log(`Sin definir: ${sindefinir}`); // undefined implícito
+console.log(`Nulo: ${nulo}`); // null
 

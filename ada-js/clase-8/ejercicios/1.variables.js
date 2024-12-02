9a98143932da409776a559113cbc31a7f1bbfdcb
joximaran <80597777+joximaran@users.noreply.github.com>
2024-12-02 20:47:26 +0000

    haciendo act 8 asincronica

--- ada-js/clase-8/ejercicios/1.variables.js
+++ ada-js/clase-8/ejercicios/1.variables.js
@@ -1,3 +1,5 @@
+//COSAS IMPORTANTES ! 
+
 // "comilla doble"    --solo texto
 // 'comilla simple'   --solo texto
 // `backticks`      --puedo concatenar texto y variable
@@ -7,4 +9,10 @@
 //Declaración de variables
 let nombre = 'Josi';
 const edad = '38';
-console.log(`Hola mi nombre es ${nombre} y tengo ${edad}anios.`);
+nombre = 'Maria Jose'
+
+console.log(`Hola mi nombre es ${nombre} y tengo ${edad}años.`);
+
+
+//cambiar valor a una variable
+console.log (`Mi nombre completo es Maria Jose`);
---
+++ ada-js/clase-8/ejercicios/2.tipo-datos.js
@@ -0,0 +1,10 @@
+//Defino distintos tipos de datos
+let numero = 18; // Numero entero
+let numero2 = 18.23; // Numero decimal
+let texto = ¨Hola universo¨; //es un string puede ser con comillas dobles o simples
+let booleano = false; // Tambien puede ser true
+let undefined =; //Indefinido
+let nulo = null; // Valor vacio
+
+console.log('Numero: {numero}');
+

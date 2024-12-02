8365960c9bd623e79d2e850ba91199fd0315ec19
joximaran <80597777+joximaran@users.noreply.github.com>
2024-12-02 22:17:26 +0000

    Corrijo error de texto sin numero pesos

--- ada-js/clase-8/ejercicios/2.tipo-datos.js
+++ ada-js/clase-8/ejercicios/2.tipo-datos.js
@@ -1,10 +1,16 @@
 //Defino distintos tipos de datos
 let numero = 18; // Numero entero
 let numero2 = 18.23; // Numero decimal
-let texto = ¨Hola universo¨; //es un string puede ser con comillas dobles o simples
+let texto = "Hola universo"; //es un string puede ser con comillas dobles o simples
 let booleano = false; // Tambien puede ser true
 let undefined =; //Indefinido
 let nulo = null; // Valor vacio
 
-console.log('Numero: {numero}');
+console.log(`Numeros: ${numero} y ${numero2}`);
+console.log(`Numero: ${numero}`);
+console.log(`texto: ${texto}`);
+console.log(`Booleano: ${booleano}`);
+console.log(`Sin definir: ${undefined}`);
+console.log(`Nulo: ${null}`);
+
 

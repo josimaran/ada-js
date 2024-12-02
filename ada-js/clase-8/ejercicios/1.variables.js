35eac649013e514cb0901174312c4a5950e43be5
joximaran <80597777+joximaran@users.noreply.github.com>
2024-12-02 20:16:48 +0000

    actualizo act 8

---
+++ ada-js/clase-8/ejercicios/1.variables.js
@@ -0,0 +1,10 @@
+// "comilla doble"    --solo texto
+// 'comilla simple'   --solo texto
+// `backticks`      --puedo concatenar texto y variable
+// ${nombre} adentro pongo el nombre de la variable q quiero mostrar
+
+
+//Declaración de variables
+let nombre = 'Josi';
+const edad = '38';
+console.log(`Hola mi nombre es ${nombre} y tengo ${edad}anios.`);

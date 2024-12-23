2b5fdf281e472a9a60bbef061a50c9e30992f24b
joximaran <mariajosemaran@gmail.com>
2024-12-23 21:25:40 +0000

    repaso y corrigo tareas clase 10

--- ada-js/clase-10/trabajos _javascript/ejercicio-1.js
+++
@@ -1,6 +0,0 @@
-const prompt = require('prompt-sync')();
-// Declaramos dos variables usando la palabra reservada "let".
-let edad = 25;  // Asignamos un valor representando la edad.
-let peso = 70;  // Asignamos un valor representando el peso.
-
-console.log(edad, peso);  // Mostramos los valores de las variables.
---
+++ ada-js/clase-10/trabajos _javascript/ejercicio1.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+// Ejercicio 1 - Declarar dos variables: Nuestra tarea será declarar dos variables: una con el nombre edad y la otra con el nombre peso. Después deberás asignarle un valor.
+
+let edad = 25;  
+let peso = 70;  
+
+// Mostrar los valores.
+console.log(`Hoy tienes ${edad} años y pesas ${peso}kg. Cambiaremos eso con nuestro plan de dieta y ejercicios.`);

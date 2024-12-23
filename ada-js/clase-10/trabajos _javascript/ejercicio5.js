53ffc1c80f42a559a8f589fbb68b6ef712936308
joximaran <mariajosemaran@gmail.com>
2024-12-23 21:53:55 +0000

    Repaso y corrijo ej clase 10

---
+++ ada-js/clase-10/trabajos _javascript/ejercicio5.js
@@ -0,0 +1,21 @@
+// 1. Declaramos y asignamos valores numéricos a dos variables
+let primerNumero = 10;  
+let segundoNumero = 25;  
+
+// 2. Declaramos las variables para los resultados de las operaciones
+let resultadoSuma;
+let resultadoResta;
+let resultadoMultiplicacion;
+let resultadoDivision;
+
+// 3. Realizamos las operaciones matemáticas y asignamos los resultados a las variables
+resultadoSuma = primerNumero + segundoNumero;  // Suma
+resultadoResta = primerNumero - segundoNumero; // Resta
+resultadoMultiplicacion = primerNumero * segundoNumero; // Multiplicación
+resultadoDivision = primerNumero / segundoNumero;  // División
+
+// 4. Imprimimos los resultados con console.log
+console.log("El resultado de la suma es: " + resultadoSuma);
+console.log("El resultado de la resta es: " + resultadoResta);
+console.log("El resultado de la multiplicación es: " + resultadoMultiplicacion);
+console.log("El resultado de la división es: " + resultadoDivision);

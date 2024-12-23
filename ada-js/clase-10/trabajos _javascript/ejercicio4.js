3c2e9c4d8780c6ce3d7f7f87575134ee26260428
joximaran <mariajosemaran@gmail.com>
2024-12-23 21:50:17 +0000

    Repaso clase 10 act clase 4

---
+++ ada-js/clase-10/trabajos _javascript/ejercicio4.js
@@ -0,0 +1,35 @@
+// 1. Declaramos una variable de tipo booleano
+let verdadero = true;  
+
+// 2. Declaramos una variable de tipo string 
+let texto = "Hola, soy Maria Jose Maran";  
+
+// 3. Declaramos una variable de tipo numérico
+let numero = 90;  
+
+// 4. Declaramos una variable null
+let nada = null;  
+
+// Operaciones y Validaciones
+
+// 1. Realiza una operación matemática con la variable numero
+let resultadoSuma = numero + 10;  // Sumar 10 al número
+console.log("Resultado de la suma: " + resultadoSuma);
+
+// 2. Concatenamos el texto con otro mensaje
+let mensajeCompleto = texto + " ¡Estoy aprendiendo JavaScript gracias a Bernie y ADA!";
+console.log(mensajeCompleto);
+
+// 3. Verificamos si la variable verdadero es true o false
+if (verdadero) {
+    console.log("La variable 'verdadero' es true.");
+} else {
+    console.log("La variable 'verdadero' es false.");
+}
+
+// 4. Verificamos si la variable nada es null
+if (nada === null) {
+    console.log("La variable 'nada' es null.");
+} else {
+    console.log("La variable 'nada' tiene un valor.");
+}

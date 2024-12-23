787c4979cf68464edf9734d75a5386ac850558bd
joximaran <mariajosemaran@gmail.com>
2024-12-23 21:58:12 +0000

    Repaso clase 10 ej 6

--- ada-js/clase-10/trabajos _javascript/ejercicio6.js
+++ ada-js/clase-10/trabajos _javascript/ejercicio6.js
@@ -1,6 +1,11 @@
 const prompt = require('prompt-sync')();
-let numero1 = 15;
-let numero2 = 20;
 
-let resultado = numero1 > 10 && numero2 > 10;  // Verificamos si ambos números son mayores que 10.
-console.log(resultado);  // Mostramos el resultado.
+// 1. Declaramos dos variables y les asignamos valores numéricos
+let numero1 = 12;
+let numero2 = 15;
+
+// 2. Usamos operadores de comparación y lógico para verificar si ambos números son mayores que 10
+let resultado = numero1 > 10 && numero2 > 10;  
+
+// 3. Mostramos el resultado en la consola
+console.log("¿Ambos números son mayores que 10?", resultado);

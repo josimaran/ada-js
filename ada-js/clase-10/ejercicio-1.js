c44046a8df439cdcf2ecee4587e2ad9fbc4f9cdd
joximaran <mariajosemaran@gmail.com>
2024-12-23 12:30:18 +0000

    -1.js

--- ada-js/clase-10/ejercicio-1.js
+++ ada-js/clase-10/ejercicio-1.js
@@ -1,3 +1,4 @@
+const prompt = require('prompt-sync')();
 // Declaramos dos variables usando la palabra reservada "let".
 let edad = 25;  // Asignamos un valor representando la edad.
 let peso = 70;  // Asignamos un valor representando el peso.

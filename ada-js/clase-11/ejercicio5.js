7035c2ad7af7ad1203c05779c7e7cd55ca9b3cbe
joximaran <mariajosemaran@gmail.com>
2024-12-24 01:47:36 +0000

    ejercicio clase 11 numero 5 practico

--- ada-js/clase-11/ejercicio5.js
+++ ada-js/clase-11/ejercicio5.js
@@ -1,19 +1,8 @@
+// Tabla de multiplicar
 const prompt = require('prompt-sync')();
 
-// Solicitamos al usuario los valores de inicio y fin
-let inicio = parseInt(prompt("Ingresa el número inicial: "));
-let fin = parseInt(prompt("Ingresa el número final: "));
+const numero = parseInt(prompt("Ingresa un número: "));
 
-// Verificamos si el rango es válido
-if (inicio > fin) {
-    console.log("El número inicial no puede ser mayor que el número final. Intenta de nuevo.");
-} else {
-    console.log(`Los números pares entre ${inicio} y ${fin} son:`);
-
-    // Usamos un ciclo for para recorrer el rango
-    for (let i = inicio; i <= fin; i++) {
-        if (i % 2 === 0) {
-            console.log(i); // Imprimimos solo los números pares
-        }
-    }
+for (let contador = 1; contador <= 10; contador++) {
+    console.log(`${numero} x ${contador} = ${numero * contador}`);
 }

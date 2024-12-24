a4f49feded86d0e7459385fb86c209c236b7a95d
joximaran <mariajosemaran@gmail.com>
2024-12-24 02:03:15 +0000

    Practico clases anteriores ej9 clase 11

---
+++ ada-js/clase-11/ejercicio9.js
@@ -0,0 +1,11 @@
+// Ejercicio 9: Cuenta regresiva para el despegue
+
+// Inicia la cuenta regresiva desde 10
+for (let numero = 10; numero >= 0; numero--) {
+    if (numero === 0) {
+        console.log("¡Despegue whoooooooo!");
+    } else {
+        console.log(numero);
+    }
+}
+

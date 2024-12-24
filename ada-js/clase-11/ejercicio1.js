a858531738abeb7593fb7b8f3c5afbf26b1a9c6e
joximaran <mariajosemaran@gmail.com>
2024-12-24 01:14:29 +0000

    Repaso y mejoro actividades clase 11 ej 1

---
+++ ada-js/clase-11/ejercicio1.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')();
+
+// Pido al usuario que ingrese un número
+let numero = parseFloat(prompt("Ingresa un número: "));
+
+// Uso el if ternario para verificar si es positivo, negativo o cero
+let mensaje = numero > 0 ? "El número es positivo." :
+             numero < 0 ? "El número es negativo." :
+             "El número es cero.";
+
+// Muestra el mensaje
+console.log(mensaje);

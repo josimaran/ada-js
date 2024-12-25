ecdba668aac350127ac1e51dbc1831c301dcb82b
joximaran <mariajosemaran@gmail.com>
2024-12-25 20:30:19 +0000

    Aprovechando el feriado para ponerme al dia con la tarea

---
+++ ada-js/clase-16/ejercicio10.js
@@ -0,0 +1,4 @@
+// Ejercicio 10: Construye una frase a partir de una lista de palabras
+let palabras = ['Me', 'gusta', 'aprender', 'JavaScript'];
+let frase = palabras.join(' '); 
+console.log(frase); 

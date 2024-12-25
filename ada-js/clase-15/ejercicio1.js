f483d4aa29d5fa3ef85c6ddf756715227af91ebf
joximaran <mariajosemaran@gmail.com>
2024-12-25 19:25:52 +0000

    repaso clase 15

---
+++ ada-js/clase-15/ejercicio1.js
@@ -0,0 +1,12 @@
+// Ejercicio 1: Añadir personajes a una lista
+
+// Lista inicial de personajes
+let personajes = ["Link", "Zelda"];
+
+// Agregamos "Mario" y "Luigi" al final de la lista usando el método push
+personajes.push("Mario");
+personajes.push("Luigi");
+
+// Mostramos el array actualizado
+console.log(personajes);
+// Resultado: ["Link", "Zelda", "Mario", "Luigi"]

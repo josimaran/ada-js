65238d56a9d9a3be74d1a5fcb1e7fe3dd030fb5d
joximaran <mariajosemaran@gmail.com>
2024-12-25 18:22:57 +0000

    ejercicio 6

---
+++ ada-js/clase-13/ejercicio6.js
@@ -0,0 +1,12 @@
+//Ejercicio 6 Convertir a minutos
+const prompt = require('prompt-sync')();
+
+// Título: Función declarada para convertir horas a minutos
+function convertirHorasAMinutos(horas) {
+    let minutos = horas * 60; // Variable local
+    return minutos; // Retorna el total en minutos
+}
+
+// Solicitar horas al usuario
+let horas = parseFloat(prompt("Ingrese la cantidad de horas: "));
+console.log("El equivalente en minutos es: " + convertirHorasAMinutos(horas));

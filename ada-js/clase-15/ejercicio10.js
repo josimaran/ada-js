673b33bc89d47b41ab01337c92fce6d80506f0f7
joximaran <mariajosemaran@gmail.com>
2024-12-25 19:56:29 +0000

    practico mucho

---
+++ ada-js/clase-15/ejercicio10.js
@@ -0,0 +1,6 @@
+// Ejercicio 10: Reemplazar una palabra en una frase
+
+let frase = "Me gusta el invierno";
+let nuevaFrase = frase.replace("invierno", "verano"); // Reemplazo "invierno" por "verano"
+console.log(nuevaFrase);
+
---
+++ ada-js/clase-15/ejercicio9.js
@@ -0,0 +1,10 @@
+// Ejercicio 9: Eliminar espacios innecesarios
+
+// Cadena original con espacios adicionales
+let saludo = "    Hola, mundo!    ";
+
+// Usamos el método trim() para eliminar los espacios al inicio y al final
+let saludoLimpio = saludo.trim();
+
+console.log("Saludo limpio:", `"${saludoLimpio}"`);
+
